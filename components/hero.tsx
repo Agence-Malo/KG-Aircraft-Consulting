ae3d202fee627e5fbb968e442dd8a93583351140
Gregory Buffard <gregory442005@gmail.com>
2024-05-24 15:51:18 +0200

    Patched gradient misplacement

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,7 +5,9 @@ import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <section
-      className={"containerize h-[100dvh] flex justify-center items-center"}
+      className={
+        "w-full h-[100dvh] flex flex-col justify-end items-center gap-[18vh]"
+      }
     >
       <video
         className={"w-full h-full object-cover object-center absolute -z-20"}
@@ -17,22 +19,24 @@ const Hero = () => {
       >
         <source src={"../graphics/videos/aerial.mp4"} type={"video/mp4"} />
       </video>
-      <div
-        className={
-          "bg-gradient-to-b from-transparent to-white absolute bottom-0 w-full h-[32vh] -z-10"
-        }
-      />
       <motion.h1
         initial={{ y: "-6vh", opacity: 0, scale: 1.05 }}
         animate={{ y: "0", opacity: 1, scale: 1 }}
         transition={{ duration: 1.5, ease: "easeOut" }}
-        className={"text-white md:text-center text-left font-normal"}
+        className={
+          "text-white md:text-center text-left font-normal containerize"
+        }
       >
         Not Just Another
         <br className={"md:block hidden"} />
         <span className={"md:hidden"}> </span>
         <span className={"font-bold"}>Business Aviation Operator</span>
       </motion.h1>
+      <div
+        className={
+          "bg-gradient-to-b from-transparent to-white w-full h-[32vh] -z-10"
+        }
+      />
     </section>
   );
 };
